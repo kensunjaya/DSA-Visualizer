dcfc64c656e690ad07539b35343ba6a9b5b5e8a2
Kenneth sunjaya <kennethsunjaya@gmail.com>
2024-09-09 00:07:06 +0700

    added edges to tree

--- src/app/tree/page.tsx
+++ src/app/tree/page.tsx
@@ -8,6 +8,14 @@ interface Tree {
   y: number;
   left?: Tree;
   right?: Tree;
+  height: number;
+}
+
+interface Edge {
+  x1: number;
+  y1: number;
+  x2: number;
+  y2: number;
 }
 
 export default function Home() {
@@ -15,7 +23,10 @@ export default function Home() {
   const [value, setValue] = useState<string>("");
   const [treeArray, setTreeArray] = useState<Tree[]>([]);
   const [root, setRoot] = useState<Tree | undefined>(undefined);
+  const [edges, setEdges] = useState<Edge[]>([]);
+
   let tempTree: Tree[] = [];
+  let tempEdges: Edge[] = [];
   let verticalDepth = 0;
   let horizontalDepth = 0;
 
@@ -29,7 +40,8 @@ export default function Home() {
       x: x,
       y: y,
       left: undefined,
-      right: undefined
+      right: undefined,
+      height: 1,
     }
     return newNode;
   }
@@ -43,37 +55,167 @@ export default function Home() {
     convertToArray(node.right);
   }
 
-  const insert = (node: Tree | undefined, value: number): Tree | undefined => {
+  const getHeight = (node: Tree | undefined) => {
+    if (node === null || node === undefined) {
+      return 0;
+    }
+    return node.height;
+  }
+
+  const getBalance = (node: Tree | undefined) => {
+    if (node === null || node === undefined) {
+      return 0;
+    }
+    return getHeight(node.left) - getHeight(node.right);
+  }
+  
+  const reposition = (node: Tree | undefined, mode: "decrease" | "increase") => {
+    if (node === null || node === undefined) {
+      return;
+    }
+    reposition(node.left, mode);
+    if (mode === "decrease") {
+      node.x -= 100;
+    }
+    else {
+      node.x += 100;
+    }
+    reposition(node.right, mode);
+  }
+
+  const createEdge = (node: Tree | undefined) => {
+    if (node === null || node === undefined) {
+      return;
+    }
+    createEdge(node.left);
+    if (node.left) {
+      const edge: Edge = {
+        x1: node.x,
+        y1: node.y,
+        x2: node.left.x,
+        y2: node.left.y,
+      }
+      tempEdges.push(edge);
+    }
+    if (node.right) {
+      const edge: Edge = {
+        x1: node.x,
+        y1: node.y,
+        x2: node.right.x,
+        y2: node.right.y,
+      }
+      tempEdges.push(edge);
+    }
+    createEdge(node.right);
+  }
+
+  const getRightHeight = (node: Tree | undefined, height: number) => {
+    if (node === null || node === undefined) {
+      return height;
+    }
+    getRightHeight(node.right, height + 1);
+  }
+
+  const getLeftHeight = (node: Tree | undefined, height: number) => {
+    if (node === null || node === undefined) {
+      return height;
+    }
+    getLeftHeight(node.left, height + 1);
+  }
+
+  const alreadyRepositioned = (node: Tree | undefined, mode: "left" | "right") => {
+    if (node === null || node === undefined) return true;
+    if (mode === "left") {
+      if (node.x === node.left?.x! + getRightHeight(node.left, 0)!*100) {
+        return true;
+      }
+      return false;
+    }
+    else {
+      if (node.x === node.right?.x! - getLeftHeight(node.right, 0)!*100) {
+        return true;
+      }
+      return false;
+    }
+  }
+
+  const insert = (node: Tree | undefined, value: number, prevNode?: Tree, mode?: "left" | "right"): Tree | undefined => {
     if (node === null || node === undefined) {
       console.log("Creating root node");
       console.log("verticalDepth: ", verticalDepth, "horizontalDepth: ", horizontalDepth);
-      const newNode = createNode(value, (screen.width / 2) + horizontalDepth*100, verticalDepth*100);
+      let newNode;
+      if (prevNode) {
+        if (mode === "left") {
+          newNode = createNode(value, prevNode.x - 100, prevNode.y + 100);
+        }
+        else {
+          newNode = createNode(value, prevNode.x + 100, prevNode.y + 100);
+        }
+      }
+      else {
+        newNode = createNode(value, screen.width / 2, 100);
+      }
       verticalDepth = horizontalDepth = 0;
       return newNode;
     }
     else if (value < node.value) {
-      console.log("entering left subtree")
+      console.log("inserting to left subtree");
       verticalDepth++;
       horizontalDepth--;
-      node.left = insert(node.left, value);
+      node.left = insert(node.left, value, node, "left");
     }
-    else {
-      console.log("entering right subtree")
+    else if (value > node.value) {
+      console.log("inserting to right subtree");
       verticalDepth++
       horizontalDepth++;
-      node.right = insert(node.right, value);
+      node.right = insert(node.right, value, node, "right");
     }
+    else {
+      return node;
+    }
+    
+    node.height = 1 + Math.max(getHeight(node.left), getHeight(node.right));
+
+    const balance = getBalance(node);
+
+    // left left case
+    if (balance > 1 && getBalance(node.left) >= 0) {
+      
+    }
+
+    // left right case
+    if (balance > 1 && getBalance(node.left) < 0) {
+      if (!alreadyRepositioned(node, "left")) {
+        reposition(node.left, "decrease");
+      }
+    }
+
+    // right right case
+    if (balance < -1 && getBalance(node.right) <= 0) {
+
+      
+    }
+
+    // right left case
+    if (balance < -1 && getBalance(node.right) > 0) {
+      if (!alreadyRepositioned(node, "right")) {
+        reposition(node.right, "increase");
+      }
+    }
+
     return node;
   }
 
   const addNode = () => {
+    tempEdges = [];
+    tempTree = [];
     const newNode = insert(root, parseInt(value));
-    console.log(newNode);
     if (newNode) {
       setRoot(newNode);
+      createEdge(newNode);
       convertToArray(newNode);
       setTreeArray([...tempTree]);
-      console.log("Tree: ", treeArray);
+      setEdges([...tempEdges]);
     }
     setValue("");
   }
@@ -86,16 +228,19 @@ export default function Home() {
         {treeArray.map((node, index) => (
           <TreeNode key={index} value={node.value} x={node.x} y={node.y} />
         ))}
-        {/* <svg className="absolute top-0 left-0 w-full h-full pointer-events-none">
+        <svg className="fixed inset-0 w-full h-full pointer-events-none z-[0]">
+        {edges.map((edge, index) => (
           <line 
-            x1={lineCoordinates.x1} 
-            y1={lineCoordinates.y1} 
-            x2={lineCoordinates.x2} 
-            y2={lineCoordinates.y2} 
-            stroke="black" 
-            strokeWidth="2" 
+            key={index}
+            x1={edge.x1 + 40} 
+            y1={edge.y1 + 40} 
+            x2={edge.x2 + 40} 
+            y2={edge.y2 + 40} 
+            stroke="#815854" 
+            strokeWidth="4" 
           />
-        </svg> */}
+        ))}
+        </svg>
       </div>
     </main>
   );
--- src/components/tree-node.tsx
+++ src/components/tree-node.tsx
@@ -9,7 +9,7 @@ interface TreeNodeProps {
 
 export const TreeNode = React.forwardRef<HTMLDivElement, TreeNodeProps>(({ value, className, x, y }, ref) => {
   return (
-    <div ref={ref} style={{left: `${x}px`, top: `${y}px`}} className={`${className} rounded-full p-5 font-play absolute border-4 text-secondary font-semibold bg-primary border-secondary flex w-fit h-fit min-w-20 min-h-20 items-center justify-center text-lg`}>
+    <div ref={ref} style={{left: `${x}px`, top: `${y}px`}} className={`${className} rounded-full z-[10] font-play fixed border-4 text-secondary font-semibold bg-primary border-secondary flex w-20 h-20 items-center justify-center text-lg`}>
       {value}
     </div>
   )
